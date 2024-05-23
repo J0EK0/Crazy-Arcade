e5b16de3c5efe2895a0be9f5900acfd6feb26564
J0EK0 <joeko921227@gmail.com>
2024-05-23 19:58:54 +0800

    delete

--- FinalProject/src/main/StartGame.java
+++
@@ -1,8 +0,0 @@
-package main;
-import gameframe.GameFrame;
-import javax.swing.*;
-public class StartGame {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(GameFrame::new);
-    }
-}
