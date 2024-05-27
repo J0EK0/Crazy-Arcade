503a25243153e32944bf943d782538745d820a1c
J0EK0 <joeko921227@gmail.com>
2024-05-27 17:35:04 +0800

    add void "move" in player

--- FinalProject/src/gameframe/GamePanel.java
+++ FinalProject/src/gameframe/GamePanel.java
@@ -27,23 +27,12 @@ import javax.swing.*;
 import model.gameobject.SuperObject;
 import thread.GameKeyListener;
 
-
-public class GamePanel extends JPanel implements Runnable {
-    //public static PlayGameMusic playmusic = new PlayGameMusic();
-    private JButton stopMusic;
-    private JButton continueMusic;
-    //private GameKeyListener keyListener;
-    private JLabel playerImg;
-    private JButton magicPao;
-    private JButton magicWei;
-    private JButton magicJiu;
-    private JButton gameControl;
-    private JButton backMain;
+public class GamePanel extends JPanel implements Runnable{
+    
     private boolean running;
     private GameKeyListener keyListener;
 
-
-    public GamePanel(){
+    public GamePanel() {
         super();
         running = true;
        // keyListener = new GameKeyListener();
@@ -59,10 +48,11 @@ public class GamePanel extends JPanel implements Runnable {
         running = true;
 
         keyListener = new GameKeyListener();
-        //this.addGameKeyListener();
-        //System.err.println("start game key listener");
+        this.addGameKeyListener();
+        System.err.println("start game key listener");
 
     }
+
     public void paint(Graphics g){
         super.paint(g);
         Gamepaint(g);
@@ -70,82 +60,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     private void  init(){
 
-        stopMusic = new JButton();
-        ImageIcon stopImg = Resourceloader.getResourceloader().getimageInfo().get("stopmusic");
-        stopImg.setImage(stopImg.getImage().getScaledInstance(150, 60, Image.SCALE_SMOOTH));
-        stopMusic.setIcon(stopImg);
-        stopMusic.setBounds(120,30,150,60);
-        stopMusic.setBorderPainted(false);
-        stopMusic.setContentAreaFilled(false);
-        //stopMusic.addActionListener(e -> stopMusicActionPerformed(e));
-
-         continueMusic = new JButton();
-         ImageIcon playImg = Resourceloader.getResourceloader().getimageInfo().get("playmusic");
-         playImg.setImage(playImg.getImage().getScaledInstance(150, 60, Image.SCALE_SMOOTH));
-         continueMusic.setIcon(playImg);
-         continueMusic.setBounds(300,30,150,60);
-         continueMusic.setBorderPainted(false);
-         continueMusic.setFocusPainted(false);
-         continueMusic.setContentAreaFilled(false);
-        // continueMusic.addActionListener(e -> continueMusicActionPerformed(e));
-
-         gameControl = new JButton();
-         ImageIcon controlImg = Resourceloader.getResourceloader().getimageInfo().get("gamecontrol");
-         controlImg.setImage(controlImg.getImage().getScaledInstance(150, 60, Image.SCALE_SMOOTH));
-         gameControl.setIcon(controlImg);
-         gameControl.setBorderPainted(false);
-         gameControl.setContentAreaFilled(false);
-         gameControl.setBounds(480, 30, 150, 60);
-        // gameControl.addActionListener(e -> gameCtrlActionPerformed(e));
-
-         backMain = new JButton();
-         ImageIcon backImg = Resourceloader.getResourceloader().getimageInfo().get("backMain");
-         backImg.setImage(backImg.getImage().getScaledInstance(150, 60, Image.SCALE_SMOOTH));
-         backMain.setIcon(backImg);
-         backMain.setBorderPainted(false);
-         backMain.setContentAreaFilled(false);
-         backMain.setFocusPainted(false);
-         backMain.setBounds(660, 30, 150, 60);
-         //backMain.addActionListener(e -> backMainBtnActionPerformed(e));
-
-         playerImg = new JLabel();
-         ImageIcon img = Resourceloader.getResourceloader().getimageInfo().get("player"+String.valueOf(StartPanel.playerIndex)+"img");
-         img.setImage(img.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
-         playerImg.setIcon(img);
-         playerImg.setBounds(950, 50, 100, 100);
-
-         magicPao = new JButton();
-         ImageIcon paoImg = Resourceloader.getResourceloader().getimageInfo().get("magicPao");
-         paoImg.setImage(paoImg.getImage().getScaledInstance(70, 70, Image.SCALE_SMOOTH));
-         magicPao.setIcon(paoImg);
-         magicPao.setBounds(950, 310, 70, 70);
-         //magicPao.addActionListener(e -> paoButtonActionPerformed(e));
-
-         magicWei = new JButton();
-         ImageIcon weiImg = Resourceloader.getResourceloader().getimageInfo().get("magicWei");
-         weiImg.setImage(weiImg.getImage().getScaledInstance(70, 70, Image.SCALE_SMOOTH));
-         magicWei.setIcon(weiImg);
-         magicWei.setBounds(950, 390, 70, 70);
-         //magicWei.addActionListener(e -> weiButtonActionPerformed(e));
-
-         magicJiu = new JButton();
-         ImageIcon jiuImg = Resourceloader.getResourceloader().getimageInfo().get("magicJiu");
-         jiuImg.setImage(jiuImg.getImage().getScaledInstance(70, 70, Image.SCALE_SMOOTH));
-         magicJiu.setIcon(jiuImg);
-         magicJiu.setBounds(950, 470, 70, 70);
-         //magicJiu.addActionListener(e -> jiuButtonActionPerformed(e));
-
-         this.setLayout(null);
-         this.add(magicPao);
-         this.add(magicWei);
-         this.add(magicJiu);
-         this.add(stopMusic);
-         this.add(backMain);
-         this.add(continueMusic);
-         this.add(gameControl);
-         this.add(playerImg);
-         this.setVisible(true);
-         this.setOpaque(true);
+        
     }
 
     /*public void stopMusicActionPerformed(ActionEvent e){
@@ -233,4 +148,16 @@ public class GamePanel extends JPanel implements Runnable {
             }
         }
     }
+
+    public void addGameKeyListener(){
+        if(keyListener != null){
+            this.removeKeyListener(keyListener);
+            this.addKeyListener(keyListener);
+        }
+    }
+    
+    public void removeGameKeyListener(){
+        this.removeKeyListener(keyListener);
+    }
+
 }
